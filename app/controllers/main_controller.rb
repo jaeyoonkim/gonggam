4653a4058aa93debff95db9020b840a172f47f6a
Dayeol Lee <loving2days@gmail.com>
2013-10-23 00:16:35 +0900

    initial commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+	def index
+		render 'index'
+	end
+	def directed
+		render 'directed'
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gongam::Application.config.secret_token = 'cc86362be813e77b453dfe14a8c27bc7e6015e60ea89f29f86e9a9933797c7325fda2d0ee2ee5873fd440d939a112f59ca84611e0c414ef3f935b744d41bfbdd'
